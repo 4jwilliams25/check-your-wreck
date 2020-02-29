c2b571a21546e80908f6a6087bc5a9200880ca34
Alexander Williams <4jwilliams25@gmail.com>
2020-02-29 13:50:33 -0700

    redux files added, get rearenders functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { useDispatch } from 'react-redux';
+import { getRearEnds } from '../src/Store/rearEnders/rearEndActions';
 
 import TopBar from './Components/Layout/TopBar';
 import NavBar from './Components/Layout/NavBar';
 import Profile from './Components/Profile/Profile';
 
 function App() {
+  
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRearEnds());
+  }, [dispatch])
+
   return (
     <Router>
       <div className="App">
